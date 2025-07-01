9e54836b925cf6de431f49852e99d33d8aa6ca58
marco gomes <marcogomesr@gmail.com>
2025-07-01 07:28:30 +0200

    feat: integrate EditorGrid and ProductLibrary components into the main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import { Button } from "@/components/ui/button";
+import EditorGrid from "./components/EditorGrid";
+import ProductLibrary from "./components/ProductLibrary";
 
 export default function Home() {
   return (
-    <main className="p-4">
-      <Button>Botón funcionando 🚀</Button>
+    <main className="max-w-7xl mx-auto py-8 px-4 grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <div className="lg:col-span-2">
+        <EditorGrid />
+      </div>
+      <div className="lg:col-span-1">
+        <ProductLibrary />
+      </div>
     </main>
   );
 }
