fab1d464163caee1a2077f2b3c4a36179afdbf91
marco gomes <marcogomesr@gmail.com>
2025-07-01 16:31:34 +0200

    feat: update metadata titles and add Toaster component for notifications in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { Inter } from "next/font/google";
 import ProviderWrapper from "@/app/ProviderWrapper";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Frontend Editor",
-  description: "Editor de categorías",
+  title: "Gestion de categorías - INDITEX",
+  description: "Gestion de categorías",
 };
 
 export default function RootLayout({
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
+        <Toaster />
         <ProviderWrapper>{children}</ProviderWrapper>
       </body>
     </html>
