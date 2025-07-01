efb288e2553ea5ec3f211662aa69cc2482393bbd
marco gomes <marcogomesr@gmail.com>
2025-07-01 07:27:49 +0200

    feat: implement hydration of grid state from localStorage in ProviderWrapper

--- src/app/ProviderWrapper.tsx
+++ src/app/ProviderWrapper.tsx
@@ -1,11 +1,19 @@
 "use client";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/store";
+import { hydrate } from "@/store/slices/gridSlice";
 
 export default function ProviderWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    const persisted = localStorage.getItem("gridState");
+    if (persisted) {
+      store.dispatch(hydrate(JSON.parse(persisted)));
+    }
+  }, []);
   return <Provider store={store}>{children}</Provider>;
 }
