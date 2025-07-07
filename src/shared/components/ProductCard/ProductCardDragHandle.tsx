2bb3dfa55f3f6ef0f0adec87f9d6abd0aebca1b6
marco gomes <marcogomesr@gmail.com>
2025-07-07 13:13:16 +0200

    refactor: replace button with Button component in ProductCardDragHandle for consistency and improved styling

--- src/shared/components/ProductCard/ProductCardDragHandle.tsx
+++ src/shared/components/ProductCard/ProductCardDragHandle.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Move } from "lucide-react";
+import { Button } from "../ui/button";
 import useIsClient from "./useIsClient";
 
 type ProductCardDragHandleProps = {
@@ -17,18 +18,18 @@ function ProductCardDragHandle({
 }: ProductCardDragHandleProps) {
   const isClient = useIsClient();
   return (
-    <button
+    <Button
       type="button"
       {...(isClient ? dragHandleProps : {})}
-      className="absolute top-2 left-2 z-20 bg-gray-200 rounded-full p-1 w-7 h-7 flex items-center justify-center cursor-grab"
-      style={{ pointerEvents: "auto" }}
+      className="absolute top-2 left-2 z-20 p-1 w-7 h-7 flex items-center justify-center cursor-grab pointer-events-auto bg-gray-100 rounded-full hover:bg-gray-200"
+      size="icon"
       tabIndex={0}
       aria-label="Arrastrar para reordenar"
       title="Arrastrar para reordenar"
       onClick={(e) => e.stopPropagation()}
     >
-      <Move className="w-4 h-4" />
-    </button>
+      <Move className="w-4 h-4 text-black" />
+    </Button>
   );
 }
 
